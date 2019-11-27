3ce8923c7a5aa4b59a5d86a854c560912d029bf3
chenzhentao <chenzhentao@baidu.com>
2019-11-27 20:36:49 +0800

    增加上报linkclicked事件demo

--- app/src/main/java/com/baidu/duer/test_botsdk/fragment/GetDeviceInfoFragment.java
+++ app/src/main/java/com/baidu/duer/test_botsdk/fragment/GetDeviceInfoFragment.java
@@ -1,17 +1,14 @@
 package com.baidu.duer.test_botsdk.fragment;
 
-import java.util.Arrays;
 import java.util.HashMap;
 
+import com.baidu.duer.bot.event.payload.LinkClickedEventPayload;
 import com.baidu.duer.botsdk.BotIntent;
 import com.baidu.duer.botsdk.BotSdk;
-import com.baidu.duer.botsdk.UiContextPayload;
 import com.baidu.duer.test_botsdk.R;
 import com.baidu.duer.test_botsdk.botsdk.BotMessageListener;
 import com.baidu.duer.test_botsdk.botsdk.IBotIntentCallback;
-import com.baidu.duer.test_botsdk.utils.BotConstants;
 import com.baidu.duer.test_botsdk.utils.DeviceInfoUtil;
-import com.baidu.duer.test_botsdk.utils.IntentDecodeUtil;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -21,7 +18,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 /**
@@ -29,7 +25,7 @@ import android.widget.TextView;
  * 1，处理自定义交互组件
  * 2，处理系统预定义交互组件，比如选择第几个，输入XX等
  */
-public class GetDeviceInfoFragment extends Fragment implements View.OnClickListener {
+public class GetDeviceInfoFragment extends Fragment implements View.OnClickListener,IBotIntentCallback {
 
 
     private TextView mResultIntentTv;
@@ -38,6 +34,7 @@ public class GetDeviceInfoFragment extends Fragment implements View.OnClickListe
     private Button mReadBduss;
     private Button mReadUserId;
     private Button mReadDeviceSerialNumber;
+    private Button mReportClickLinkEvent;
 
     private static final String TAG = "HandleIntentFragment";
     public GetDeviceInfoFragment() {
@@ -70,6 +67,8 @@ public class GetDeviceInfoFragment extends Fragment implements View.OnClickListe
         mReadUserId.setOnClickListener(this);
         mReadDeviceSerialNumber = view.findViewById(R.id.read_device_serial_number);
         mReadDeviceSerialNumber.setOnClickListener(this);
+        mReportClickLinkEvent = view.findViewById(R.id.report_link_clicked_event);
+        mReportClickLinkEvent.setOnClickListener(this);
     }
 
 
@@ -77,12 +76,14 @@ public class GetDeviceInfoFragment extends Fragment implements View.OnClickListe
     public void onAttach(Context context) {
         Log.i(TAG, "on fragment attach");
         super.onAttach(context);
+        BotMessageListener.getInstance().addCallback(this);
     }
 
     @Override
     public void onDetach() {
         Log.i(TAG, "on fragment detach");
         super.onDetach();
+        BotMessageListener.getInstance().removeCallback(this);
     }
 
 
@@ -104,6 +105,29 @@ public class GetDeviceInfoFragment extends Fragment implements View.OnClickListe
             case R.id.get_pass_info:
                 mResultIntentTv.setText(DeviceInfoUtil.getUserInfo(DeviceInfoUtil.PATH_PASS_INFO));
                 break;
+            case R.id.report_link_clicked_event:
+                LinkClickedEventPayload linkClickedEventPayload = new LinkClickedEventPayload();
+                linkClickedEventPayload.url =
+                        "dueros://6402fc3a-7825-3147-b52e-c25911b6d03c/entity/swan/add?recommendId=recommend_habit_operative_xiashuoyingwen&habitDetail=%7b%22repeatDays%22%3a%5b%22MON%22%2c%22TUE%22%2c%22WED%22%2c%22THU%22%2c%22FRI%22%2c%22SAT%22%2c%22SUN%22%5d%2c%22startTime%22%3a%2208%3a18%22%2c%22duration%22%3a0%2c%22enableTriggerMode%22%3atrue%2c%22triggerMode%22%3a2%2c%22enableAutoClose%22%3atrue%2c%22autoCloseOptionSelected%22%3a%7b%22text%22%3a%2290%e5%88%86%e9%92%9f%22%2c%22id%22%3a0%2c%22activated%22%3atrue%7d%7d&callback=callback__o_0__d934a2128b5944648639e4e06c3ad737cb&habitChannel=THIRD_PARTY_DUYAYA_TEST";
+                BotSdk.getInstance().uploadLinkClickedEvent(linkClickedEventPayload);
+                break;
         }
+    }
+
+    @Override
+    public void handleIntent(BotIntent intent, String customData) {
+        String intentResult = getString(R.string.result_intent) + "\n指令名称:%s\n槽位信息：%s\n自定义信息：%s";
+        mResultIntentTv.setText(String.format(intentResult, intent.name, intent.slots, customData));
+
+    }
+
+    @Override
+    public void onClickLink(String url, HashMap<String, String> paramMap) {
+
+    }
+
+    @Override
+    public void onHandleScreenNavigatorEvent(int event) {
+
     }
 }
